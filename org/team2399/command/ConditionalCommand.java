7c268bc2c003684caa667af12c4c4964c2ca4bbe
Will Marshall <willcodymarshall@gmail.com>
2017-05-30 19:39:09 -0700

    reworkd

--- org/team2399/command/ConditionalCommand.java
+++ org/team2399/command/ConditionalCommand.java
@@ -7,158 +7,171 @@
 
 package org.team2399.command;
 
-import java.util.Enumeration;
-
 /**
- * A {@link ConditionalCommand} is a {@link Command} that starts one of two commands.
+ * A {@link ConditionalCommand} is a {@link Command} that starts one of two
+ * commands.
  *
  * <p>
- * A {@link ConditionalCommand} uses m_condition to determine whether it should run m_onTrue or
- * m_onFalse.
+ * A {@link ConditionalCommand} uses m_condition to determine whether it should
+ * run m_onTrue or m_onFalse.
  * </p>
  *
  * <p>
- * A {@link ConditionalCommand} adds the proper {@link Command} to the {@link Scheduler} during
- * {@link ConditionalCommand#initialize()} and then {@link ConditionalCommand#isFinished()} will
- * return true once that {@link Command} has finished executing.
+ * A {@link ConditionalCommand} adds the proper {@link Command} to the
+ * {@link Scheduler} during {@link ConditionalCommand#initialize()} and then
+ * {@link ConditionalCommand#isFinished()} will return true once that
+ * {@link Command} has finished executing.
  * </p>
  *
  * <p>
- * If no {@link Command} is specified for m_onFalse, the occurrence of that condition will be a
- * no-op.
+ * If no {@link Command} is specified for m_onFalse, the occurrence of that
+ * condition will be a no-op.
  * </p>
  *
  * @see Command
  * @see Scheduler
  */
 public abstract class ConditionalCommand extends Command {
-  /**
-   * The Command to execute if {@link ConditionalCommand#condition()} returns true.
-   */
-  private Command m_onTrue;
+	/**
+	 * The Command to execute if {@link ConditionalCommand#condition()} returns
+	 * true.
+	 */
+	private Command m_onTrue;
 
-  /**
-   * The Command to execute if {@link ConditionalCommand#condition()} returns false.
-   */
-  private Command m_onFalse;
+	/**
+	 * The Command to execute if {@link ConditionalCommand#condition()} returns
+	 * false.
+	 */
+	private Command m_onFalse;
 
-  /**
-   * Stores command chosen by condition.
-   */
-  private Command m_chosenCommand = null;
+	/**
+	 * Stores command chosen by condition.
+	 */
+	private Command m_chosenCommand = null;
 
-  /**
-   * Creates a new ConditionalCommand with given onTrue and onFalse Commands.
-   *
-   * <p>Users of this constructor should also override condition().
-   *
-   * @param onTrue The Command to execute if {@link ConditionalCommand#condition()} returns true
-   */
-  public ConditionalCommand(Command onTrue) {
-    this(onTrue, new InstantCommand());
-  }
+	/**
+	 * Creates a new ConditionalCommand with given onTrue and onFalse Commands.
+	 *
+	 * <p>
+	 * Users of this constructor should also override condition().
+	 *
+	 * @param onTrue
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns true
+	 */
+	public ConditionalCommand(Command onTrue) {
+		this(onTrue, new InstantCommand());
+	}
 
-  /**
-   * Creates a new ConditionalCommand with given onTrue and onFalse Commands.
-   *
-   * <p>Users of this constructor should also override condition().
-   *
-   * @param onTrue The Command to execute if {@link ConditionalCommand#condition()} returns true
-   * @param onFalse The Command to execute if {@link ConditionalCommand#condition()} returns false
-   */
-  public ConditionalCommand(Command onTrue, Command onFalse) {
-    m_onTrue = onTrue;
-    m_onFalse = onFalse;
+	/**
+	 * Creates a new ConditionalCommand with given onTrue and onFalse Commands.
+	 *
+	 * <p>
+	 * Users of this constructor should also override condition().
+	 *
+	 * @param onTrue
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns true
+	 * @param onFalse
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns false
+	 */
+	public ConditionalCommand(Command onTrue, Command onFalse) {
+		m_onTrue = onTrue;
+		m_onFalse = onFalse;
 
-    for (Enumeration e = m_onTrue.getRequirements(); e.hasMoreElements(); ) {
-      requires((Subsystem) e.nextElement());
-    }
+		m_onTrue.getRequirementsStream().forEach(this::requires);
+		m_onFalse.getRequirementsStream().forEach(this::requires);
+	}
 
-    for (Enumeration e = m_onFalse.getRequirements(); e.hasMoreElements(); ) {
-      requires((Subsystem) e.nextElement());
-    }
-  }
+	/**
+	 * Creates a new ConditionalCommand with given name and onTrue and onFalse
+	 * Commands.
+	 *
+	 * <p>
+	 * Users of this constructor should also override condition().
+	 *
+	 * @param name
+	 *            the name for this command group
+	 * @param onTrue
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns true
+	 */
+	public ConditionalCommand(String name, Command onTrue) {
+		this(name, onTrue, new InstantCommand());
+	}
 
-  /**
-   * Creates a new ConditionalCommand with given name and onTrue and onFalse Commands.
-   *
-   * <p>Users of this constructor should also override condition().
-   *
-   * @param name the name for this command group
-   * @param onTrue The Command to execute if {@link ConditionalCommand#condition()} returns true
-   */
-  public ConditionalCommand(String name, Command onTrue) {
-    this(name, onTrue, new InstantCommand());
-  }
+	/**
+	 * Creates a new ConditionalCommand with given name and onTrue and onFalse
+	 * Commands.
+	 *
+	 * <p>
+	 * Users of this constructor should also override condition().
+	 *
+	 * @param name
+	 *            the name for this command group
+	 * @param onTrue
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns true
+	 * @param onFalse
+	 *            The Command to execute if
+	 *            {@link ConditionalCommand#condition()} returns false
+	 */
+	public ConditionalCommand(String name, Command onTrue, Command onFalse) {
+		super(name);
+		m_onTrue = onTrue;
+		m_onFalse = onFalse;
 
-  /**
-   * Creates a new ConditionalCommand with given name and onTrue and onFalse Commands.
-   *
-   * <p>Users of this constructor should also override condition().
-   *
-   * @param name the name for this command group
-   * @param onTrue The Command to execute if {@link ConditionalCommand#condition()} returns true
-   * @param onFalse The Command to execute if {@link ConditionalCommand#condition()} returns false
-   */
-  public ConditionalCommand(String name, Command onTrue, Command onFalse) {
-    super(name);
-    m_onTrue = onTrue;
-    m_onFalse = onFalse;
+		m_onTrue.getRequirementsStream().forEach(this::requires);
+		m_onFalse.getRequirementsStream().forEach(this::requires);
+	}
 
-    for (Enumeration e = m_onTrue.getRequirements(); e.hasMoreElements(); ) {
-      requires((Subsystem) e.nextElement());
-    }
+	/**
+	 * The Condition to test to determine which Command to run.
+	 *
+	 * @return true if m_onTrue should be run or false if m_onFalse should be
+	 *         run.
+	 */
+	protected abstract boolean condition();
 
-    for (Enumeration e = m_onFalse.getRequirements(); e.hasMoreElements(); ) {
-      requires((Subsystem) e.nextElement());
-    }
-  }
+	/**
+	 * Calls {@link ConditionalCommand#condition()} and runs the proper command.
+	 */
+	@Override
+	protected void _initialize() {
+		if (condition()) {
+			m_chosenCommand = m_onTrue;
+		} else {
+			m_chosenCommand = m_onFalse;
+		}
 
-  /**
-   * The Condition to test to determine which Command to run.
-   *
-   * @return true if m_onTrue should be run or false if m_onFalse should be run.
-   */
-  protected abstract boolean condition();
+		// This is a hack to make cancelling the chosen command inside a
+		// CommandGroup work properly
+		m_chosenCommand.clearRequirements();
 
-  /**
-   * Calls {@link ConditionalCommand#condition()} and runs the proper command.
-   */
-  @Override
-  protected void _initialize() {
-    if (condition()) {
-      m_chosenCommand = m_onTrue;
-    } else {
-      m_chosenCommand = m_onFalse;
-    }
+		m_chosenCommand.start();
+	}
 
-    // This is a hack to make cancelling the chosen command inside a CommandGroup work properly
-    m_chosenCommand.clearRequirements();
+	@Override
+	protected void _cancel() {
+		if (m_chosenCommand != null && m_chosenCommand.isRunning()) {
+			m_chosenCommand.cancel();
+		}
 
-    m_chosenCommand.start();
-  }
+		super._cancel();
+	}
 
-  @Override
-  protected void _cancel() {
-    if (m_chosenCommand != null && m_chosenCommand.isRunning()) {
-      m_chosenCommand.cancel();
-    }
+	@Override
+	protected boolean isFinished() {
+		return m_chosenCommand != null && m_chosenCommand.isRunning() && m_chosenCommand.isFinished();
+	}
 
-    super._cancel();
-  }
+	@Override
+	protected void interrupted() {
+		if (m_chosenCommand != null && m_chosenCommand.isRunning()) {
+			m_chosenCommand.cancel();
+		}
 
-  @Override
-  protected boolean isFinished() {
-    return m_chosenCommand != null && m_chosenCommand.isRunning()
-        && m_chosenCommand.isFinished();
-  }
-
-  @Override
-  protected void interrupted() {
-    if (m_chosenCommand != null && m_chosenCommand.isRunning()) {
-      m_chosenCommand.cancel();
-    }
-
-    super.interrupted();
-  }
+		super.interrupted();
+	}
 }
